6753e18b735ca8c2b5dbafbaecf6100b6e9fe47c
Morski1337 <26685032+Dungeon-Morski@users.noreply.github.com>
2023-05-16 21:15:27 +0300

    Add files via upload

---
+++ modules/ring3/CookieStorage.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosInstance } from 'axios';
+import { uuid } from 'uuidv4';
+
+export class CookieUserSession {
+    public Axios: AxiosInstance;
+    constructor () {
+        const test = uuid();
+    }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
+export default class CookieStorage {
+    // eslint-disable-next-line @typescript-eslint/no-useless-constructor
+    constructor () {}
+    private static 
+};
